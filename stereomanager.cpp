78d841807e0b9b067da4475dd1aff29afe1a25d2
Alexander Brock <Brock.Alexander@web.de>
2023-06-03 23:51:17 +0200

    Measure execution time

--- stereomanager.cpp
+++ stereomanager.cpp
@@ -5,6 +5,8 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 
+#include <ParallelTime/paralleltime.h>
+
 StereoManager::Method StereoManager::str2method(std::string str) {
     str = Misc::to_lower(str);
     if ("simple" == str) {
@@ -55,13 +57,24 @@ void StereoManager::optimize() {
 }
 
 void StereoManager::run() {
+    std::string const name_red_cyan = "red-cyan";
+    std::string const name_red_cyan_ce = "red-cyan, contrast enhanced";
+    static cv::Mat empty(cam_target_l->size, CV_8UC1, cv::Scalar(0,0,0));
+    cv::imshow(name_red_cyan, empty);
+    cv::imshow(name_red_cyan_ce, empty);
+    cv::waitKey(1);
     optimize();
 
+    std::cout << "Left cam:  " << cam_l->print() << std::endl;
+    std::cout << "Right cam: " << cam_r->print() << std::endl;
+
+    ParallelTime t;
     std::cout << "Generating maps..." << std::flush;
     cv::Mat2f map_l = cam_l->simCamMap(cam_target_l);
     cv::Mat2f map_r = cam_r->simCamMap(cam_target_r);
-    std::cout << "done." << std::endl;
+    std::cout << "done: " << t.print() << std::endl;
 
+    t.start();
     auto clahe = cv::createCLAHE(4.0, {8,8});
 
     cv::Mat img_orig_l_grey, img_orig_r_grey;
@@ -71,7 +84,9 @@ void StereoManager::run() {
     cv::Mat img_orig_ce_l_grey, img_orig_ce_r_grey;
     clahe->apply(img_orig_l_grey, img_orig_ce_l_grey);
     clahe->apply(img_orig_r_grey, img_orig_ce_r_grey);
+    std::cout << "Applying CLAHE: " << t.print() << std::endl;
 
+    t.start();
     cv::Mat img_l, img_r, img_l_grey, img_r_grey, img_l_ce, img_r_ce;
     std::cout << "Remapping..." << std::flush;
     cv::remap(*cam_l->img, img_l, map_l, cv::noArray(), cv::INTER_LANCZOS4);
@@ -82,12 +97,14 @@ void StereoManager::run() {
 
     cv::remap(img_orig_ce_l_grey, img_l_ce, map_l, cv::noArray(), cv::INTER_LANCZOS4);
     cv::remap(img_orig_ce_r_grey, img_r_ce, map_r, cv::noArray(), cv::INTER_LANCZOS4);
-    std::cout << "done." << std::endl;
+    std::cout << "done: " << t.print() << std::endl;
 
+    t.start();
     cv::Mat red_cyan = Misc::merge_red_cyan(img_l_grey, img_r_grey);
     cv::Mat red_cyan_ce = Misc::merge_red_cyan(img_l_ce, img_r_ce);
+    std::cout << "Merging: " << t.print() << std::endl;
 
-    cv::imshow("red-cyan", red_cyan);
-    cv::imshow("red-cyan, contrast enhanced", red_cyan_ce);
+    cv::imshow(name_red_cyan, red_cyan);
+    cv::imshow(name_red_cyan_ce, red_cyan_ce);
     cv::waitKey(1);
 }
